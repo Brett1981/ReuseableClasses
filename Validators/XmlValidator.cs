12ac0cea48587168803ebcb14924878155a3fee5
Stephen Brett <stephenbrett@btinternet.com>
2015-10-09 16:53:12 +0100

    XmlValidator.cs added

---
+++ Validators/XmlValidator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace Re_useable_Classes.Validators
+{
+    public class XmlValidator
+    {
+        string _schemaFileName = "";
+        List<Exception> _errors = null;
+
+        public XmlValidator(string schemaFileName)
+        {
+            _schemaFileName = schemaFileName;
+            validateInputs();
+        }
+
+        void validateInputs()
+        {
+            if (!File.Exists(_schemaFileName))
+                throw new InvalidOperationException(string.Format("XML schema '{0}' does not exist", _schemaFileName));
+        }
+
+        public List<Exception> ValidateXml(XDocument doc)
+        {
+            _errors = new List<Exception>();
+
+            var schemaSet = new XmlSchemaSet();
+            schemaSet.Add(null, _schemaFileName);
+
+            doc.Validate(schemaSet, new ValidationEventHandler(validationEvent));
+
+            return _errors;
+        }
+
+        void validationEvent(object sender, ValidationEventArgs eventArgs)
+        {
+            _errors.Add(eventArgs.Exception);
+        }
+
+    }
+}
