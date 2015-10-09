90a08d5f81bb95664eba3235d7b613d51e6ada57
Stephen Brett <stephenbrett@btinternet.com>
2015-10-09 17:24:22 +0100

    CheckInputDataRow

---
+++ SQL/CheckInputDataRow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Re_useable_Classes.SQL
+{
+    public class CheckInputDataRow
+    {
+        public bool DoesRecordExist(SqlConnection ConnectionString, string SelectStatement)
+        {
+            SqlConnection cnConn = ConnectionString;
+            SqlDataAdapter daExist =
+                new SqlDataAdapter(SelectStatement, cnConn);
+            DataTable dtExists = new DataTable();
+            bool recordExists = false;
+
+            daExist.Fill(dtExists);
+
+            if (dtExists.Rows.Count > 0)
+            {
+                recordExists = true;
+            }
+
+            return recordExists; 
+        }
+    }
+}
