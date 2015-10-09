2d13d56bd61e909efafa8127aa14f60140b5b421
Stephen Brett <stephenbrett@btinternet.com>
2015-10-09 17:16:33 +0100

    FormatDataString Used to format single quotes found in strings for sql insert

---
+++ Functions/FormatDataString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Re_useable_Classes.Functions
+{
+    public class FormatDataString
+    {
+        /// <summary>
+        /// Used to format single quotes found in strings for sql insert
+        /// </summary>
+        /// <param name="InputString"></param>
+        /// <returns>Formatted String</returns>
+        public static string FormatSingleQuotes(string InputString)
+        {
+            string result = null;
+            if (InputString.Contains("'"))
+            {
+                result = InputString.Replace("'", "''");
+            }
+
+            return result;
+        }
+    }
+}
